9b1a1615fee2171747aab2149a79ea73118d9073
TheEpicBlock <git.teb@theepicblock.nl>
2023-12-18 13:53:38 +0100

    More stuff

---
+++ src/client/java/nl/theepicblock/fluiwid/client/mixin/AttachWaterData.java
@@ -0,0 +1,36 @@
+package nl.theepicblock.fluiwid.client.mixin;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.util.ClientPlayerTickable;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import nl.theepicblock.fluiwid.FishyBusiness;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(ClientPlayerEntity.class)
+public abstract class AttachWaterData extends PlayerEntity {
+    @Shadow @Final private List<ClientPlayerTickable> tickables;
+    @Unique
+    private FishyBusiness waterData;
+
+    public AttachWaterData(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
+        super(world, pos, yaw, gameProfile);
+    }
+
+    @Inject(method = "init", at = @At("RETURN"))
+    private void onInit(CallbackInfo ci) {
+        // TODO only add water data if the player is actually liquid
+        this.waterData = new FishyBusiness(this);
+        this.tickables.add(() -> this.waterData.tick());
+    }
+}
--- src/main/java/nl/theepicblock/fluiwid/FishyBusiness.java
+++ src/main/java/nl/theepicblock/fluiwid/FishyBusiness.java
@@ -1,5 +1,6 @@
 package nl.theepicblock.fluiwid;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Vec3d;
 
 import java.util.ArrayList;
@@ -9,10 +10,16 @@ public class FishyBusiness {
      * Keeps track of water particles
      */
     private final SpatialStructure<Droplet> particles = new SpatialStructure<>();
+    private final PlayerEntity player;
 
-    public FishyBusiness() {
+    public FishyBusiness(PlayerEntity player) {
+        this.player = player;
         for (int i = 0; i < 50; i++) {
-            particles.insert(Vec3d.ZERO, new Droplet());
+            particles.insert(player.getPos(), new Droplet());
         }
+    }
+
+    public void tick() {
+
     }
 }
