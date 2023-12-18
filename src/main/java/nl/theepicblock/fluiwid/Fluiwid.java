1cea032e66934344fd92a4f511bb6f86c860483f
TheEpicBlock <git.teb@theepicblock.nl>
2023-12-18 13:36:02 +0100

    Init repo

---
+++ src/main/java/nl/theepicblock/fluiwid/Fluiwid.java
@@ -0,0 +1,20 @@
+package nl.theepicblock.fluiwid;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Fluiwid implements ModInitializer {
+	private static final String MOD_ID = "fluiwid";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
