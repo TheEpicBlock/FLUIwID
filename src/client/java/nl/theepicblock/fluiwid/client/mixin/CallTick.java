c8c97de3797520a9f682496bc22c10db662abde6
TheEpicBlock <git.teb@theepicblock.nl>
2023-12-30 16:16:12 +0100

    Toggle based on status effect

--- src/client/java/nl/theepicblock/fluiwid/client/mixin/CallTick.java
+++ src/client/java/nl/theepicblock/fluiwid/client/mixin/CallTick.java
@@ -23,34 +23,33 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.List;
 
 @Mixin(ClientPlayerEntity.class)
-public abstract class AttachWaterData extends PlayerEntity implements PlayerDuck {
+public abstract class CallTick extends PlayerEntity {
     @Shadow @Final private List<ClientPlayerTickable> tickables;
     @Shadow public Input input;
-    @Unique
-    private FishyBusiness waterData;
 
-    public AttachWaterData(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
+    public CallTick(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
         super(world, pos, yaw, gameProfile);
     }
 
     @Inject(method = "init", at = @At("RETURN"))
     private void onInit(CallbackInfo ci) {
-        // TODO only add water data if the player is actually liquid
-        this.waterData = new FishyBusiness(this);
-        this.tickables.add(() -> this.waterData.tick());
+        this.tickables.add(() -> {
+            var waterData = ((PlayerDuck)this).fluiwid$getData();
+            if (waterData != null) {
+                waterData.tick();
+            }
+        });
     }
 
     @Inject(method = "tickMovement", at = @At("RETURN"))
     private void tickMovement(CallbackInfo ci) {
-        var x = this.input.movementSideways;
-        var z = this.input.movementForward;
-        float sin = MathHelper.sin(this.getYaw() * 0.017453292F);
-        float cos = MathHelper.cos(this.getYaw() * 0.017453292F);
-        this.waterData.movementVec = new Vec3d(x * (double)cos - z * (double)sin, 0, z * (double)cos + x * (double)sin);
-    }
-
-    @Override
-    public @Nullable FishyBusiness fluiwid$getData() {
-        return this.waterData;
+        var waterData = ((PlayerDuck)this).fluiwid$getData();
+        if (waterData != null) {
+            var x = this.input.movementSideways;
+            var z = this.input.movementForward;
+            float sin = MathHelper.sin(this.getYaw() * 0.017453292F);
+            float cos = MathHelper.cos(this.getYaw() * 0.017453292F);
+            waterData.movementVec = new Vec3d(x * (double)cos - z * (double)sin, 0, z * (double)cos + x * (double)sin);
+        }
     }
 }
--- src/main/java/nl/theepicblock/fluiwid/mixin/AttachWaterData.java
+++ src/main/java/nl/theepicblock/fluiwid/mixin/AttachWaterData.java
@@ -1,38 +1,37 @@
 package nl.theepicblock.fluiwid.mixin;
 
-import com.mojang.authlib.GameProfile;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import nl.theepicblock.fluiwid.FishyBusiness;
+import nl.theepicblock.fluiwid.Fluiwid;
 import nl.theepicblock.fluiwid.PlayerDuck;
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
-
-@Mixin(ServerPlayerEntity.class)
-public abstract class AttachWaterData extends PlayerEntity implements PlayerDuck {
+@Mixin(PlayerEntity.class)
+public abstract class AttachWaterData extends LivingEntity implements PlayerDuck {
     @Unique
     private FishyBusiness waterData;
 
-    public AttachWaterData(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
-        super(world, pos, yaw, gameProfile);
+    protected AttachWaterData(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
     }
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void onInit(CallbackInfo ci) {
-        // TODO only add water data if the player is actually liquid
-        this.waterData = new FishyBusiness(this);
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void onTick(CallbackInfo ci) {
+        if (this.hasStatusEffect(Fluiwid.WATER_EFFECT)) {
+            if (this.waterData == null) {
+                this.waterData = new FishyBusiness((PlayerEntity)(Object)this);
+            }
+        } else {
+            this.waterData = null;
+        }
     }
 
     @Override
