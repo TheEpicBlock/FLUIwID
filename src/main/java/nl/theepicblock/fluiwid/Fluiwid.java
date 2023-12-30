98441433ea0306fe02665b7f86abd238cae63b8d
TheEpicBlock <git.teb@theepicblock.nl>
2023-12-30 15:37:25 +0100

    Add potion effect

--- src/main/java/nl/theepicblock/fluiwid/Fluiwid.java
+++ src/main/java/nl/theepicblock/fluiwid/Fluiwid.java
@@ -2,6 +2,15 @@ package nl.theepicblock.fluiwid;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
+import net.minecraft.recipe.BrewingRecipeRegistry;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,8 +19,14 @@ public class Fluiwid implements ModInitializer {
 	private static final String MOD_ID = "fluiwid";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final StatusEffect WATER_EFFECT = Registry.register(Registries.STATUS_EFFECT, id("water"), new WaterStatusEffect(StatusEffectCategory.BENEFICIAL, 0x9654f9));
+	public static final Potion WATER_POTION = Registry.register(Registries.POTION, id("fluiwid"), new Potion(new StatusEffectInstance(WATER_EFFECT, 3600)));
+	public static final Potion LONG_WATER_POTION = Registry.register(Registries.POTION, id("long_fluiwid"), new Potion("fluiwid", new StatusEffectInstance(WATER_EFFECT, 9600*2)));
+
 	@Override
 	public void onInitialize() {
+		BrewingRecipeRegistry.registerPotionRecipe(Potions.AWKWARD, Items.WATER_BUCKET, WATER_POTION);
+		BrewingRecipeRegistry.registerPotionRecipe(WATER_POTION, Items.GUNPOWDER, LONG_WATER_POTION);
 	}
 
 	public static Identifier id(String path) {
---
+++ src/main/java/nl/theepicblock/fluiwid/WaterStatusEffect.java
@@ -0,0 +1,10 @@
+package nl.theepicblock.fluiwid;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
+public class WaterStatusEffect extends StatusEffect {
+    protected WaterStatusEffect(StatusEffectCategory category, int color) {
+        super(category, color);
+    }
+}
