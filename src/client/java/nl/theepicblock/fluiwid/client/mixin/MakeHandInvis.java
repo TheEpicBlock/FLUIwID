03ec9d73d22f44f973a8a1274210f58a48e0457c
TheEpicBlock <git.teb@theepicblock.nl>
2023-12-31 18:31:42 +0100

    Fix invis of hand

--- src/client/java/nl/theepicblock/fluiwid/client/mixin/MakeHandInvis.java
+++ src/client/java/nl/theepicblock/fluiwid/client/mixin/MakeHandInvis.java
@@ -3,6 +3,7 @@ package nl.theepicblock.fluiwid.client.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import nl.theepicblock.fluiwid.PlayerDuck;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,7 +14,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 public class MakeHandInvis {
     @WrapOperation(method = "renderFirstPersonItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;isInvisible()Z"))
     private boolean changeInvis(AbstractClientPlayerEntity instance, Operation<Boolean> original) {
-        if (this instanceof PlayerDuck quack && quack.fluiwid$getData() != null) {
+        if (instance instanceof PlayerDuck quack && quack.fluiwid$getData() != null) {
+            return true;
+        }
+        return original.call(instance);
+    }
+
+    @WrapOperation(method = {"renderMapInOneHand", "renderMapInBothHands"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isInvisible()Z"))
+    private boolean changeInvis2(ClientPlayerEntity instance, Operation<Boolean> original) {
+        if (instance instanceof PlayerDuck quack && quack.fluiwid$getData() != null) {
             return true;
         }
         return original.call(instance);
