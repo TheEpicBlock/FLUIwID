8b595f5b77810487f8966327138a35c09600765a
TheEpicBlock <git.teb@theepicblock.nl>
2024-01-01 01:03:24 +0100

    AAA

--- src/main/java/nl/theepicblock/fluiwid/mixin/AttachWaterData.java
+++ src/main/java/nl/theepicblock/fluiwid/mixin/AttachWaterData.java
@@ -2,7 +2,11 @@ package nl.theepicblock.fluiwid.mixin;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
 import nl.theepicblock.fluiwid.FishyBusiness;
 import nl.theepicblock.fluiwid.Fluiwid;
@@ -18,14 +22,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class AttachWaterData extends LivingEntity implements PlayerDuck {
     @Unique
     private FishyBusiness waterData;
+    @Unique
+    private static final TrackedData<Boolean> IS_WATER = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
 
     protected AttachWaterData(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
 
+    @Inject(method = "initDataTracker", at = @At("HEAD"))
+    private void aa(CallbackInfo ci) {
+        this.dataTracker.set(IS_WATER, false);
+
+    }
+
     @Inject(method = "tick", at = @At("HEAD"))
     private void onTick(CallbackInfo ci) {
-        if (this.hasStatusEffect(Fluiwid.WATER_EFFECT)) {
+        if (((Object)this) instanceof ServerPlayerEntity) {
+            this.dataTracker.set(IS_WATER, this.hasStatusEffect(Fluiwid.WATER_EFFECT));
+        }
+        if (this.dataTracker.get(IS_WATER)) {
             if (this.waterData == null) {
                 this.waterData = new FishyBusiness((PlayerEntity)(Object)this);
             }
